466d36866e12cf5e252df364bff509ac41003e64
Kythadrin <vadimsiric@gmail.com>
2024-12-09 17:02:25 +0200

    Created api endpoint for get client by id

--- Backend/src/Controller/API/ClientController.php
+++ Backend/src/Controller/API/ClientController.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Controller\API;
 
 use App\DTO\Input\Client as ClientInput;
+use App\DTO\Output\Client as ClientOutput;
 use App\Service\ClientService;
+use App\Entity\Client;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Attributes as OA;
 
-class Client
+class ClientController
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
@@ -31,7 +34,7 @@ class Client
             schema: new OA\Schema(type: 'string', example: 'Client created successfully!')
         )
     )]
-    public function createClient(ClientInput $client): Response
+    public function create(ClientInput $client): Response
     {
         try {
             $this->clientService->createClient($client);
@@ -42,5 +45,31 @@ class Client
         }
 
         return new Response('Client created successfully!', Response::HTTP_CREATED);
+    }
+
+    #[Route(path: '/api/client/{id}', methods: ['GET'])]
+    #[OA\Get(description: 'Get a client by id.', summary: 'Get a client')]
+    #[OA\Parameter(name: 'id', description: 'The ID of the client to retrieve', in: 'path', required: true)]
+    #[OA\Response(
+        response: Response::HTTP_OK,
+        description: 'Client created successfully',
+        content: new OA\JsonContent(
+            ref: ClientOutput::class
+        )
+    )]
+    public function get(Client $client): JsonResponse
+    {
+        try {
+            $clientData = new ClientOutput(
+                $client->getId(),
+                $client->getTelegramId(),
+                $client->getBalance(),
+            );
+
+        } catch (Exception $exception) {
+            return new JsonResponse('Something went wrong!', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return new JsonResponse($clientData, Response::HTTP_OK);
     }
 }
--- Backend/src/DTO/Output/Client.php
+++ Backend/src/DTO/Output/Client.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\DTO\Input;
+namespace App\DTO\Output;
 
 use OpenApi\Attributes as OA;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -10,10 +10,16 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Client
 {
     public function __construct(
+        #[OA\Property(description: 'Client ID', type: 'integer')]
+        public int $id,
         #[OA\Property(description: 'Telegram ID of the client', type: 'string', example: '123456789')]
         #[Assert\NotBlank]
         #[Assert\Regex(pattern: "/^\d+$/", message: "Telegram ID must be numeric.")]
         public string $telegramId,
+        #[OA\Property(description: 'Client balance', type: 'string', example: '1000.00')]
+        #[Assert\NotBlank]
+        #[Assert\Positive]
+        public string $balance,
     ) {
     }
 }
